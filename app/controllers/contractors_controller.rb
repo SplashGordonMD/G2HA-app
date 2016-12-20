b3fb5825256285d9c0c718cdaf0f96978f2f2b26
Casey Platt <kse0824@gmail.com>
2016-12-20 12:19:33 -0600

    1219

--- app/controllers/contractors_controller.rb
+++ app/controllers/contractors_controller.rb
@@ -25,7 +25,9 @@ class ContractorsController < ApplicationController
 
   def show
     @contractor = Contractor.find_by(id: params[:id])
-    render "show.html.erb" 
+    render "show.html.erb"
+
+    @contractor_reviews=Review.find_by(contractor_id: @contractor.id) 
   end
 
   def edit
