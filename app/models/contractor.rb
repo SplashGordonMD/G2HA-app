f3268b3b44c27368fda6a77d0ec1726b4017415e
Casey Platt <kse0824@gmail.com>
2017-01-11 19:35:03 -0600

    prefered award css

--- app/models/contractor.rb
+++ app/models/contractor.rb
@@ -4,15 +4,11 @@ class Contractor < ApplicationRecord
   has_many :contractor_job_categories
   has_many :job_categories, through: :contractor_job_categories
 
-  def preferred
-  	star_count = 0
-
-  	reviews.each do |review|
-  		star_count += review.stars
-  	end
-
-  	if star_count >= 8
-  		"Preferred"
-  	end
+  def preferred?
+    star_count = 0
+    reviews.each do |review|
+  	   star_count += review.stars
+    end
+    star_count >= 8	
   end
 end
