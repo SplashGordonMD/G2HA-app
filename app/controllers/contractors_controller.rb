d837a67008cae4daa64643d6d00ac115451f81d3
Casey Platt <kse0824@gmail.com>
2017-01-16 19:44:16 -0600

    clearer routing update

--- app/controllers/contractors_controller.rb
+++ app/controllers/contractors_controller.rb
@@ -1,4 +1,5 @@
 class ContractorsController < ApplicationController
+  before_action :authenticate_user!
   
   def index 
     @contractors = Contractor.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/' => 'pages#home'
-  get '/' => 'contractors#index'
+  # get '/' => 'contractors#index'
   get '/contractors' => 'contractors#index'
   get '/contractors/new' => 'contractors#new'
   post '/contractors' => 'contractors#create'
